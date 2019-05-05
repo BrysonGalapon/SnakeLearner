8d6bad7f20b67f41bb2dd9b3192185703e42c426
BrysonGalapon <bgalapon@mit.edu>
2019-05-04 18:01:18 -0700

    Updated Main Driver

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from NN_Manager import NN_Manager
 from NeuralNetwork import NeuralNetwork
 
 if __name__ == "__main__":
-    print("Main!")
-    nn = NeuralNetwork()
+    num_inputs = 11
+    num_outputs = 4
+    num_generations = 10
 
+    # Create the grandfaher NNs
+    adam = NeuralNetwork(num_inputs, num_outputs)
+    eve = NerualNetwork(num_inputs, num_outputs)
+
+    nn_manager = NN_Manager(adam, eve)
+
+    # leave NNs for a while
+    nn_manager.evolve(num_generations)
